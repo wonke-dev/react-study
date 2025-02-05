776ca8b8ec7a36f9150a9d502a48c80f93303b4c
wonke-dev <wonk0311@gmail.com>
2025-02-05 23:49:27 +0900

    day11 - 배달 음식 주문 페이지 만들기

--- project01/src/App.jsx
+++ project01/src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
 
-function Welcome({ name, isMember }) {
-  return (
-    <h1>
-      {isMember ? `${name}님 다시 오셨군요` : `${name}님 가입하시겠어요?`}
-    </h1>
-  );
-}
+import OrderEditor from "./components/OrderEditor";
 
 function App() {
-  return <Welcome isMember={true} name={"부웡이"} />;
+  return (
+    <>
+      <OrderEditor />
+    </>
+  );
 }
 
 export default App;
